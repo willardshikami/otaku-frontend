16eae7e5b3370a35d1af9340ec02d191db10d37e
willardshikami <willardshikami@gmail.com>
2017-06-14 13:55:48 +0300

    removed vue, created db, created users & routes

--- app.js
+++ app.js
@@ -1,40 +1,53 @@
 const path = require('path');
 const express = require('express');
-const expressVue = require('express-vue');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors')
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//connecting to the database
+mongoose.connect(config.database);
+
+//confirming if connection is successful
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database '+ config.database);
+});
+
+//checking for errors
+mongoose.connection.on('error', (err) => {
+    console.log('Database error '+ err);
+});
+
 const app = express();
 
+//route
 const users = require('./routes/users');
 
-app.engine('vue', expressVue);
-app.set('view engine', 'vue');
-app.set('views', path.join(__dirname, '/views'));
-app.set('vue', {
-    componentsDir: path.join(__dirname, '/views/components'),
-    defaultLayout: 'layout'
-});
 
 //Cors Middleware to make request to API on different domains
 app.use(cors());
 
-//Body parser middleware
+//Body parser middleware to accept data
 app.use(bodyParser.json());
 
+//static folder
+app.use(express.static(path.join(__dirname, 'client')))
 
-//
+
+//user models
 app.use('/users', users);
 
-//Route to homepage
-app.get('/', function (req, res) {
-    var scope = {
-        data: {},
-        vue: {}
-    };
-    res.render('index', scope);
+
+//port var
+const port = 5000;
+
+//route to homepage(invalid)
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
 });
 
-app.listen(5000);
-console.log('Express server listening on port 5000');
+//listening to and starting the server on port 5000
+app.listen(port, () => {
+    console.log('Server started on port ' + port)
+});
