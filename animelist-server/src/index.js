cc21992453e3ef4a666725dd2e1d66348dce91fb
willardshikami <willardshikami@gmail.com>
2017-09-28 12:08:08 +0300

    babel setup to run the node server using ES6-7

--- animelist-server/src/index.js
+++ animelist-server/src/index.js
@@ -2,6 +2,12 @@ import express from 'express';
 import path from 'path';
 const app = express();
 
+
+//route to API
+app.post('/api/auth', (req, res) => {
+  res.status(400).json({errors: {global: 'invalid credentials'}});
+});
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
