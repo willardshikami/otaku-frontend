fa3ec86c02a1d7e823c9ea818eb6775352dc92dc
willardshikami <willardshikami@gmail.com>
2018-07-03 10:45:02 +0300

    updated UI

--- otaku-client/src/app/events/events.component.ts
+++ otaku-client/src/app/events/events.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-events',
+  selector: 'events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
--- otaku-client/src/app/explore/explore.component.ts
+++ otaku-client/src/app/explore/explore.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-explore',
+  selector: 'explore',
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.css']
 })
